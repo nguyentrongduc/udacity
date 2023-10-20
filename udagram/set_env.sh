5371789549959d7613d9508106edea265031c4bb
nguyen trong duc <nguyentrongduc111@gmail.com>
2023-10-20 22:45:04 +0700

    set env

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=myPassword
-export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=postgres.c0jpbeti5wus.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
+export AWS_BUCKET=arn:aws:s3:::udagram-final
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
-export URL=http://localhost:8100
+export URL=http://udagram-api-dev-ver-2.eba-vajbtwgd.us-east-1.elasticbeanstalk.com/
