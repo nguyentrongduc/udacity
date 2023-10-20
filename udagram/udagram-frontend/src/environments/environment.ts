2c3fbf2267de3429e26ffe8d1f929319b7817a03
nguyen trong duc <nguyentrongduc111@gmail.com>
2023-10-21 00:26:44 +0700

    Deployzz

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  appName: "Udagram",
+  appName: 'Udagram',
   apiHost:
-    "http://udagram-api-dev-ver-2.eba-vajbtwgd.us-east-1.elasticbeanstalk.com/api/v0",
+    'http://udagram-api-dev-ver-2.eba-vajbtwgd.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
